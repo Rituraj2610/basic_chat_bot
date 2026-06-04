9d21d7e04f63034559502a63ff5528144c4000df
Rituraj2610 <26rituraj@gmail.com>
2026-06-04 09:59:13 +0530

    [Rituraj] ADD . Integrated frontend with backend

--- src/langgraph_agentic_ai/main.py
+++ src/langgraph_agentic_ai/main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
+from src.langgraph_agentic_ai.llms.groqllm import GroqLLM
 from src.langgraph_agentic_ai.ui.streamlit.loadui import LoadStreamlitUI
+from src.langgraph_agentic_ai.llms.groqllm import GroqLLM
+from src.langgraph_agentic_ai.graph.graph_builder import GraphBuilder
+from src.langgraph_agentic_ai.ui.streamlit.display_result import DisplayResultStreamlit
 
 def load_langgraph_agentic_ai_app():
     """
@@ -20,4 +24,32 @@ def load_langgraph_agentic_ai_app():
         return
 
     user_message = st.chat_input("Enter your message")
+
+    if user_message:
+        try:
+            # CONfigure llms
+            obj_llm_config=GroqLLM(user_controls_input=user_input)
+            model=obj_llm_config.get_llm_model()
+
+            if not model:
+                st.error("Error: llm model could not be initialized")
+                return
+
+            usecase = user_input.get("selected_usecases")
+
+            if not usecase:
+                st.error("Error: No use case selected")
+                return
+
+            ## Graph Builder
+            graph_builder = GraphBuilder(model=model)
+            try:
+                graph= graph_builder.setup_graph(usecase)
+                DisplayResultStreamlit(usecase, graph, user_message).display_result_on_ui()
+            except Exception as e:
+                st.error(f"Graph setup failed -{e}")
+                return
+        except Exception as e:
+            st.error(f"No user message -{e}")
+            return
 
