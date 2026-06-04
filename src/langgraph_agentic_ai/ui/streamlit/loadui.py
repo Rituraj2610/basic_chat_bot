2b071a41cc92596e66f02b93f8f96de7802c063b
Rituraj2610 <26rituraj@gmail.com>
2026-06-04 09:06:03 +0530

    [Rituraj] ADD . Created frontend using streamlit

---
+++ src/langgraph_agentic_ai/ui/streamlit/loadui.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import os
+
+from src.langgraph_agentic_ai.ui.uiconfigfile import Config
+
+class LoadStreamlitUI:
+    def __init__(self):
+        self.config=Config()
+        self.user_controls={}
+
+    def load_streamlit_ui(self):
+        st.set_page_config(page_title=self.config.get_page_title(), layout="wide")
+        st.header(self.config.get_page_title())
+
+        with st.sidebar:
+            llm_options = self.config.get_llm_options()
+            usecase_options = self.config.get_usecase_options()
+
+            self.user_controls["selected_llm"] = st.selectbox("Select llm", llm_options)
+
+            if self.user_controls["selected_llm"] == 'Groq':
+                model_options = self.config.get_groq_model_options()
+                self.user_controls["selected_groq_model"] = st.selectbox("Select model", model_options)
+                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"] = st.text_input("API Key", type="password")
+
+                # Validate API Key
+
+                if not self.user_controls["GROQ_API_KEY"]:
+                    st.warning("Please enter groq api key to proceed")
+
+
+            #  USECASE
+
+            self.user_controls["selected_usecases"] = st.selectbox("Select usecases", usecase_options)
+
+        return self.user_controls
